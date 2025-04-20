06fa2fd8ac9342348bb365eb735bea2b9d96ea5c
ezra_rl25 <ezra.klauber@roxburylatin.org>
2025-04-20 19:00:47 -0400

    Removed old src folder with old API integration files

--- src/API_Integration/main/java/AthletesApiExample.java
+++
@@ -1,9 +0,0 @@
-import com.strava.api.v3.*;
-import com.strava.api.v3.auth.*;
-import com.strava.api.v3.model.*;
-import com.strava.api.v3.api.AthletesApi;
-
-import rx.Observable;
-
-    public class AthletesApiExample {
-    }
--- src/API_Integration/main/java/StravaIntegration.java
+++
@@ -1,5 +0,0 @@
-public class StravaIntegration {
-    public static void main(String[] args) {
-
-    }
-}
