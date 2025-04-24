f44371d1ad7cfa394501b17e532e13b775af8283
kmuni12 <krishmuni12@gmail.com>
2025-04-24 00:54:34 -0400

    Added more methods for analyzing activities

--- javastrava_API-Integration/src/APIImplementation/ActivityAnalyzer.java
+++ javastrava_API-Integration/src/APIImplementation/ActivityAnalyzer.java
@@ -12,6 +12,7 @@ public class ActivityAnalyzer {
     private double totalElevation = 0;
     private double acuteLoad = 0;
     private double chronicLoad=0;
+    private double trimp=0;
 
     public ActivityAnalyzer(Strava api) {
         this.api = api;
@@ -31,20 +32,32 @@ public class ActivityAnalyzer {
     public void updateWeeklyMetrics(LocalDateTime endOfWeek) {
         List<StravaActivity> activities = getActivitiesInWeek(endOfWeek);
         for (StravaActivity act : activities) {
+            totalElevation=0;
+            totalDistance=0;
+            totalMovingTime=0;
+            acuteLoad=0;
+            trimp=0;
             totalElevation += act.getTotalElevationGain();
             totalDistance += act.getDistance();
             totalMovingTime += act.getMovingTime();
             acuteLoad += (act.getDistance() * act.getMovingTime());
+            trimp += (act.getAverageHeartrate()*act.getElapsedTime());
         }
     }
 
     public void updateMonthlyMetrics(LocalDateTime endOfMonth) {
         List<StravaActivity> activities = getActivitiesInMonth(endOfMonth);
         for (StravaActivity act : activities) {
+            totalElevation=0;
+            totalDistance=0;
+            totalMovingTime=0;
+            chronicLoad=0;
+            trimp=0;
             totalElevation += act.getTotalElevationGain();
             totalDistance += act.getDistance();
             totalMovingTime += act.getMovingTime();
             chronicLoad += (act.getDistance() * act.getMovingTime());
+            trimp += (act.getAverageHeartrate()*act.getElapsedTime());
         }
     }
 
@@ -63,6 +76,16 @@ public class ActivityAnalyzer {
     public double getChronicLoad () {
         return chronicLoad;
     }
+
+    public double getMonthlyTrimp(LocalDateTime endOfMonth) {
+        updateMonthlyMetrics(endOfMonth);
+        return trimp;
+    }
+    public double getWeeklyTrimp(LocalDateTime endOfWeek) {
+        updateWeeklyMetrics(endOfWeek);
+        return trimp;
+    }
+
     public double getACR(LocalDateTime endOfMonth) {
         updateWeeklyMetrics(endOfMonth);
         double acuteLoad=getAcuteLoad();
@@ -70,11 +93,43 @@ public class ActivityAnalyzer {
         double chronicLoad=getChronicLoad();
         return (acuteLoad/chronicLoad);
     }
+
     public double getTSB(LocalDateTime endOfMonth) {
         updateWeeklyMetrics(endOfMonth);
         double acuteLoad=getAcuteLoad();
         updateMonthlyMetrics(endOfMonth);
         double chronicLoad=getChronicLoad();
         return (chronicLoad-acuteLoad);
+    }
+
+    public double getMonthConsistency(LocalDateTime endOfMonth) {
+        updateMonthlyMetrics(endOfMonth);
+        int activityNumber=0;
+        double meanLoad=0;
+        double totalDistance=0;
+        double totalMovingTime=0;
+
+        List<StravaActivity> activities = getActivitiesInMonth(endOfMonth);
+        for (StravaActivity act : activities) {
+            totalMovingTime+=act.getMovingTime();
+            totalDistance=+act.getDistance();
+            activityNumber++;
+        }
+
+        meanLoad=totalMovingTime*totalDistance/activityNumber;
+
+        double varianceSum = 0;
+
+        for (StravaActivity act : activities) {
+            varianceSum += Math.pow((act.getDistance()*act.getMovingTime()) - meanLoad, 2);
+        }
+
+        double standardDeviation = Math.sqrt(varianceSum / activityNumber);
+
+        // Consistency score
+        double consistency = 1 - (standardDeviation / meanLoad);
+
+        // Clamp result between 0 and 1
+        return Math.max(0, Math.min(1, consistency));
     }
 }
